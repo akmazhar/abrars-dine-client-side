bf82b3c358d0cb08012c9a2fa5efa5c7a055d602
এখানে আপনার github এর user name দিবেন <akmazharshuvo07@gmail.com>
2023-11-08 14:16:47 +0600

    front cards are added namely each card

--- src/Pages/EachCard/EachCard.jsx
+++ src/Pages/EachCard/EachCard.jsx
@@ -1,11 +1,25 @@
 
 
-const EachCard = () => {
-    return (
-        <div>
+
+        const EachCard = ({ fsixc }) => {
             
-        </div>
-    );
-};
+            const { food_image, food_name, food_category, price } = fsixc;
+
+
+            return (
+                <div className="card card-compact w-96 bg-base-100 mt-3 shadow-xl">
+                <figure><img src={food_image} alt="" /></figure>
+                <div className="card-body">
+                  <h2 className="card-title">{food_name}</h2>
+                  <h2 className="card-title">{food_category}</h2>
+                  <p className="font-bold text-lg text-red-500">Price : $ {price}</p>
+                  <div className="card-actions justify-end">
+                    
+                  </div>
+                </div>
+              </div>
+            );
+        };
+
 
 export default EachCard;
